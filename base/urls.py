ce4dc948d6a41c1805fc531f13fc40bb83f6cfb9
Adriel <adriel.fariasan@gmail.com>
2022-03-15 22:42:35 -0300

    Define rotas CRUD de sala

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('salas/<str:pk>', views.room, name='room')
+    path('salas/criar', views.create_room, name='create-room'),
+    path('salas/editar/<str:pk>', views.update_room, name='update-room'),
+    path('salas/apagar/<str:pk>', views.delete_room, name='delete-room'),
+    path('salas/<str:pk>', views.room, name='room'),
 ]
