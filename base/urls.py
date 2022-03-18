6fdbcfaaa1b23c98c7a2099ac8fe754463605bf0
Adriel <adriel.fariasan@gmail.com>
2022-03-17 22:55:40 -0300

    Renomeia sala para squad nas urls

--- base/urls.py
+++ base/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('salas/criar', views.create_room, name='create-room'),
-    path('salas/editar/<str:pk>', views.update_room, name='update-room'),
-    path('salas/apagar/<str:pk>', views.delete_room, name='delete-room'),
-    path('salas/<str:pk>', views.room, name='room'),
+    path('squads/criar', views.create_room, name='create-room'),
+    path('squads/editar/<str:pk>', views.update_room, name='update-room'),
+    path('squads/apagar/<str:pk>', views.delete_room, name='delete-room'),
+    path('squads/<str:pk>', views.room, name='room'),
 ]
