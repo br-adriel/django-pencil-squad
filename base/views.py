258bc462ef5c12a3d3675469ceeb6b3bdba64e83
Adriel <adriel.fariasan@gmail.com>
2022-03-17 22:52:46 -0300

    Implementa busca de salas

--- base/views.py
+++ base/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from django.shortcuts import redirect, render
+
 from .forms import RoomForm
+from .models import Room, Topic
 
 
 def home(request):
-    rooms = Room.objects.all()
-    return render(request, 'base/home.html', {'rooms': rooms})
+    q = ''
+    if request.GET.get('q') != None:
+        q = request.GET.get('q')
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics': topics}
+    return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
