34567ec106540808bcc72783c6431802cad58f91
Adriel <adriel.fariasan@gmail.com>
2022-03-19 23:28:27 -0300

    Adiciona rota para cadastro

--- base/urls.py
+++ base/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
     path('login', views.login_page, name="login"),
     path('logout', views.logout_user, name="logout"),
+    path('register', views.register_user, name="register"),
+
     path('squads/criar', views.create_room, name='create-room'),
     path('squads/editar/<str:pk>', views.update_room, name='update-room'),
     path('squads/apagar/<str:pk>', views.delete_room, name='delete-room'),
