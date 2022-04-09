b58fcf94c2089ee0cd9a3db1e8fcf54456777e98
Adriel <adriel.fsantos@outlook.com>
2022-04-09 11:50:51 -0300

    Adiciona view getRooms

--- base/api/views.py
+++ base/api/views.py
@@ -1,5 +1,8 @@
+from base.models import Room
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .serializers import RoomSerializer
 
 
 @api_view(['GET'])
@@ -10,3 +13,10 @@ def getRoutes(request):
         'GET /api/rooms/:id',
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+def getRooms(request):
+    rooms = Room.objects.all()
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
